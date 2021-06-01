3f93ae1cbbf6577552d0374e068c153b68a807dd
Lorenzo <Lorenzo@LORENZO.homenet.telecomitalia.it>
2021-06-01 11:03:13 +0200

    A

--- src/main/java/model/TestSimulator.java
+++ src/main/java/model/TestSimulator.java
@@ -4,6 +4,12 @@ public class TestSimulator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Simulator sim = new Simulator();
+		
+		sim.init();
+		sim.run();
+		System.out.println(sim.getPatientsDead()+" Death, "+sim.getPatientsAbandoned()+" Abandoned, "+sim.getPatientsTreated()+" Treated.");
 
 	}
 
